583c239f2ec04072afcf7dd4e0ba904145f6eca5
unknown <lambdafate@163.com>
2019-12-04 20:11:50 +0800

    login-register

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
-from flask import Flask, session, url_for, redirect, request, render_template
+from flask import Flask, session, url_for, redirect, request, render_template, jsonify
 from app.extensions import db, migrate
 from app.model import Driver, Consigner, Good, Application
 from app import config
 from app.utils import login_required
+import pysnooper
+import os
 
 app = Flask(__name__)
+
+basedir = os.path.abspath('.')
+logdir = os.path.abspath("./log")
 
 app.config.from_object(config)
 db.init_app(app=app)
@@ -14,6 +19,37 @@ migrate.init_app(app=app, db=db)
 def index():
     return render_template("index.html")
 
-@app.route("/login/")
+@app.route("/login/", methods=["GET", "POST"])
+@pysnooper.snoop(output=f"{logdir}/login.log")
 def login():
-    return "login!"
+    if(request.method == "POST"):
+        data = {
+            "code": "200"
+            ,"msg": "登录成功!"
+            ,"url": "/index/"
+        }
+        
+        phone = request.form.get("phone")
+        password = request.form.get("password")
+
+        user = Driver.query.filter_by(phone_number = phone).first() 
+        if user is None:
+            user = Consigner.query.filter_by(phone_number = phone).first()
+
+        islogin = False
+        if user and user.check_password(password):
+            islogin = True
+            session["islogin"] = islogin
+            session["role"] = user.user_type
+            session["username"] = user.username
+            data["msg"] = f"登陆成功, 你的角色为{session['role']}!"
+        else:
+            data["msg"] = "登录失败"
+        return jsonify(data)
+    return render_template("login.html")
+
+
+@app.route("/register/")
+def register():
+    return render_template("register.html")
+

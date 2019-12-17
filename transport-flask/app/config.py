e20087e72a85912957013d4681d1697ee84c7b6a
ONE-creator <56661971+ONE-creator@users.noreply.github.com>
2019-12-17 11:02:04 +0800

    first

--- transport-flask/app/config.py
+++ transport-flask/app/config.py
@@ -8,3 +8,4 @@ SECRET_KEY = os.urandom(24)
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://transport:transport@39.107.83.159:3306/" \
                           "transport"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+# gioa

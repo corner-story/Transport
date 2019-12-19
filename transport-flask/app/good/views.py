92174fc91d558831029d74e1780358f7e4f904c4
unknown <lambdafate@163.com>
2019-12-19 21:57:14 +0800

    add api(/good/id)

--- transport-flask/app/good/views.py
+++ transport-flask/app/good/views.py
@@ -3,7 +3,7 @@ from flask import request, jsonify
 from app.models import Good
 from app.extensions import db
 from app.utils import login_required
-# import pysnooper
+import pysnooper
 
 
 # 获取所有可利用的，即处于"等待司机承运"状态的货物
@@ -26,12 +26,11 @@ def get_available_goods():
 
     goods = Good.query.filter_by(isactive=1).order_by(
         Good.timestamp.desc()).paginate(page, limit, error_out=False)
-    
+
     goods = [
         {
             "id": good.id,
-            "good_name": good.good_name, 
-            "good_type": good.good_type,
+            "good_name": good.good_name,
             "transport_origin": good.transport_origin,
             "transport_des": good.transport_des,
             "transport_money": good.transport_money,
@@ -41,4 +40,35 @@ def get_available_goods():
     ]
     res["data"] = goods
 
+    return jsonify(res)
+
+
+@good.route("/good/")
+@login_required
+def get_one_good():
+    res = {
+        "state": "success", "msg": "get good successfully", "data": []
+    }
+    good_id = request.args.get("id")
+    # 查找对应的货物
+    good = Good.query.filter_by(id=good_id).first()
+
+    if(good == None):
+        res["state"] = "error"
+        res["msg"] = "error"
+        return jsonify(res)
+
+    data = {
+        "id": good.id,
+        "good_name": good.good_name,
+        "transport_origin": good.transport_origin,
+        "transport_des": good.transport_des,
+        "transport_money": good.transport_money,
+        "backup": good.backup,
+        "consigner": good.consigner.username,
+        "consigner_id": good.consigner.id,
+        "phone_number": good.consigner.phone_number
+    }
+
+    res["data"] = data
     return jsonify(res)
